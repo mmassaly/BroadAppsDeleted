fd7b814f66c45a77c9a947b53b58fb1d5456c4ac
mmassaly <77269347+mmassaly@users.noreply.github.com>
2023-10-05 16:38:04 +0000

    Update index.js

--- EntryExitsBaseControlleratMSA/index.js
+++ EntryExitsBaseControlleratMSA/index.js
@@ -6,7 +6,7 @@ var connection = http.createServer(function(req,res)
 	
 	res.writeHead(200,{"Content-Type": "text/html","Access-Control-Allow-Origin":"*"
 				,"Access-Control-Allow-Methods":"POST, GET, PUT, DELETE, OPTIONS","Access-Control-Allow-Credentials":false});
-	faire_un_simple_query("Select prenom,nom from * individu").then((results)=>{
+	faire_un_simple_query("Select prenom,nom from individu").then((results)=>{
 		res.write("<h1>"+"Hello "+results.first.prenom+" "+results.first.nom+"</h1>");
 		res.end();
 	},(error)=>
