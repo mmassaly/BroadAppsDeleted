f9a7e68f9935b1f450b855ddb8bc0314bf0c14ab
mmassaly <77269347+mmassaly@users.noreply.github.com>
2023-10-05 17:42:50 +0000

    Update index.js

--- MCV/index.js
+++ MCV/index.js
@@ -586,11 +586,12 @@
 								}
 								else
 								{
-									// res.writeHead(500, {"Content-Type": "application/json","Access-Control-Allow-Origin":"*"
-												// ,"Access-Control-Allow-Methods":"POST, GET, PUT, DELETE, OPTIONS","Access-Control-Allow-Credentials":false
-												// ,"Access-Control-Max-Age":'86400'
-												// ,"Access-Control-Allow-Headers":"X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-												// });
+									console.log("You are at area with response 500");
+									res.writeHead(500, {"Content-Type": "application/json","Access-Control-Allow-Origin":"*"
+												,"Access-Control-Allow-Methods":"POST, GET, PUT, DELETE, OPTIONS","Access-Control-Allow-Credentials":false
+												,"Access-Control-Max-Age":'86400'
+												,"Access-Control-Allow-Headers":"X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+												});
 									res.end();
 								}
 							}	
