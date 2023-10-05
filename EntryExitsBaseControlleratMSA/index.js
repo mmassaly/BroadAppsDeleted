5ae2763dd3efac2bb46c1fbc001e1b4a9bc4b766
mmassaly <77269347+mmassaly@users.noreply.github.com>
2023-10-05 16:25:26 +0000

    Commits

--- EntryExitsBaseControlleratMSA/index.js
+++ EntryExitsBaseControlleratMSA/index.js
@@ -1,4 +1,5 @@
 const http = require("http");
+var postgres = require('pg');
 
 var connection = http.createServer(function(req,res)
 {
@@ -11,3 +12,84 @@ var connection = http.createServer(function(req,res)
 
 connection.listen(process.env.PORT || 3035);
 //require('dotenv').config();
+
+
+async function faire_un_simple_query(queryString)
+	{
+		let sql = await exigencebasededonnée();	
+		console.log(queryString);
+		
+		try
+		{
+			
+			let result = await sql.query(queryString);
+			
+			try
+			{
+				await sql.end();
+			}
+			catch(ex){}
+			
+			return new Promise ((resolve,reject) => 
+			{
+						
+				if(result == undefined)
+				{
+					reject({first:result,second:false});
+				}
+				else
+				{
+					if(result.rows == undefined)
+					{
+						let tempfirst = [];
+						result.forEach((element)=>
+						{
+							tempfirst.push({first: element.rows,second:element.fields});
+						});
+						
+						resolve(tempfirst);
+					}
+					else
+					resolve({first:result.rows,second:result.fields});
+				}
+						
+			});
+		}
+		catch(ex)
+		{
+			try
+			{
+				await sql.end();
+			}
+			catch(e)
+			{
+				
+			}
+			console.log("Exception caught");
+			return new Promise((resolve,reject)=>{reject({first:ex,second:false});});
+		}		
+	}
+	
+	
+	async function exigencebasededonnée()
+	{	
+		try 
+		{	
+			var postgresConnection = new postgres.Client("postgres://default:QHiOur92EwzF@ep-patient-darkness-72544749.us-east-1.postgres.vercel-storage.com:5432/verceldb"+ "?sslmode=require");
+			await postgresConnection.connect();
+			return new Promise ((resolve,reject) => 
+			{
+				console.log("Database connection successfull.");
+				resolve(postgresConnection);	
+			});
+		}
+		catch(ex)
+		{
+			return new Promise ((resolve,reject) => 
+			{
+				console.log("Database Connection not successful.");	
+				reject(postgresConnection);
+			});
+		}
+		//console.log(postgresConnection);
+	}
