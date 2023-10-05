1ad51900c28ce83d422981a4ef0b6e36edea6393
mmassaly <77269347+mmassaly@users.noreply.github.com>
2023-10-05 15:50:34 +0000

    Update index.js

--- EntryExitsBaseControlleratMSA/index.js
+++ EntryExitsBaseControlleratMSA/index.js
@@ -6,14 +6,14 @@ var connection = http.createServer(function(req,res)
 {
 	res.writeHead(200,{"Content-Type": "text/html","Access-Control-Allow-Origin":"*"
 				,"Access-Control-Allow-Methods":"POST, GET, PUT, DELETE, OPTIONS","Access-Control-Allow-Credentials":false});
-	faire_un_simple_query("select id from individu;").then((result)=>{
+	/*faire_un_simple_query("select id from individu;").then((result)=>{*/
 		res.write("<p>"+result[0]+"</p>");
 		res.end();
-	},(error)=>
+	/*},(error)=>
 	{
 		res.write("<p>"+error.first+"</p>");
 		res.end();
-	});
+	});*/
 });
 
 connection.listen(process.env.PORT || 3035);
