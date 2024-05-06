f160c64dba7213df54756ea44cacb46d103c8d14
mmassaly <77269347+mmassaly@users.noreply.github.com>
2024-05-06 14:57:28 +0000

    Update index.js

--- MCV/api/index.js
+++ MCV/api/index.js
@@ -996,6 +996,15 @@
 						});
 					}
 				}
+				else
+				{
+					res.writeHead(200, {"Content-Type": "application/json","Access-Control-Allow-Origin":"*"
+						,"Access-Control-Allow-Methods":"POST, GET, PUT, DELETE, OPTIONS","Access-Control-Allow-Credentials":false
+						,"Access-Control-Max-Age":'86400'
+						,"Access-Control-Allow-Headers":"X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+					});
+					res.end();
+				}
 			});
 	
 	function whileFunction(startingTag)
