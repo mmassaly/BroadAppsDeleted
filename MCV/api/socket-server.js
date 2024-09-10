92bc2984ab4c487ac8d197469108081ff6b4b112
mmassaly <77269347+mmassaly@users.noreply.github.com>
2024-09-10 15:52:00 +0000

    Go

--- MCV/api/socket-server.js
+++ MCV/api/socket-server.js
@@ -2,6 +2,8 @@ var http = require("http");
 var { Server } = require('socket.io');
 var url = require("url");	
 const connected_guys = {};
+var fs = require("fs");
+var callIndex = 0;
 
 var server2 = http.createServer(function(req,res)
 {
@@ -56,13 +58,134 @@ var server2 = http.createServer(function(req,res)
 					});
 				}
 			}
-			res.writeHead(200, {"Content-Type": "application/json","Access-Control-Allow-Origin":"*"
-				,"Access-Control-Allow-Methods":"POST, GET, PUT, DELETE, OPTIONS","Access-Control-Allow-Credentials":false
-				,"Access-Control-Max-Age":'86400'
-				,"Access-Control-Allow-Headers":"X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-			});
-			res.write(JSON.stringify({PASSED:true}));
-			res.end();
+			
+			var returnValid = false;
+			callIndex++;
+			//console.log("Call index is "+callIndex);
+			var imageType = "";
+			if(req.url.endsWith(".jpeg"))
+				imageType = "jpeg";
+			if(req.url.endsWith(".png"))
+				imageType = "png";
+			if(req.url.endsWith(".jpg"))
+				imageType = "jpg";
+			
+			if(req.url.endsWith(".jpeg") ||req.url.endsWith(".png") || req.url.endsWith(".jpg") || req.url.endsWith(".ico"))
+			{
+				returnValid = true;
+				var imageUrlReprocessed = req.url.substring(1,req.url.length).replaceAll("%20"," ");
+				imageUrlReprocessed = decodeURI(imageUrlReprocessed);
+				console.log("You bot are making an image request processed to be "+imageUrlReprocessed);
+				
+				let responseb = res;
+				fs.exists(imageUrlReprocessed,async function(exists)
+				{
+					//console.log("exists = "+exists);
+					if(exists)
+					{
+						fs.readFile(imageUrlReprocessed,function(err,data)
+						{
+							if(err)
+							{
+								//console.log(err);
+								responseb.end();
+							}
+							else if(data)
+							{
+								responseb.writeHeader(200,{"Content-Type":"image/"+imageType});
+								responseb.write(data);
+								responseb.end();
+							}
+						});
+					}
+					else
+					{
+						try
+						{
+							
+							imageDictionary.findUrlBufferValue("https://msa-pointage-server.onrender.com/"+imageUrlReprocessed).then((result)=>{
+								responseb.writeHeader(200,{"Content-Type":"image/"+imageType});
+								console.log(result);
+								responseb.write(result);
+								responseb.end();
+							}
+							,(err)=>
+							{
+								responseb.writeHeader(200,{"Content-Type":"image/"+imageType});
+								console.log(err);
+								responseb.write(err);
+								responseb.end();
+							});
+						}
+						catch(ex)
+						{
+							console.log(ex);
+							responseb.end();
+						}
+						
+						//console.log("Image file does not exist.");
+					}
+				});
+			}
+			else
+			{
+				console.log("Down here");
+				console.log(req.url);
+				if(req.url.endsWith("MCV"))
+				{
+					//console.log("Before MCV file");
+					returnValid = true;
+					fs.readFile("SelfDescription.htm",function(err,data)
+					{
+						if(data != undefined)
+						{
+							res.writeHeader(200,{"Content-Type":"text/html"});
+							res.write(data);
+							res.end();
+						}
+						else if(err != undefined)
+						{
+							res.writeHeader(200,{"Content-Type":"text/html"});
+							res.end();
+						}
+					});
+				}
+				else if(req.url.endsWith("Download/pointage-apk"))
+				{
+					returnValid = true;
+					console.log("inside Download");
+					let resb = res;
+					fs.readFile("File/app-pointage-msa.apk",(error,data)=>
+					{
+						if(error)
+						{
+							console.log(error);
+							resb.writeHead(500,{"Content-Type":"application/vnd.android.package-archive"});
+							resb.end();
+						}
+						else
+						{
+							console.log(data);
+							resb.writeHead(200,{"Content-Type":"application/vnd.android.package-archive"});
+							resb.write(data);
+							resb.end();
+							console.log("Done sending apk...");
+						}
+					});
+				}
+				
+			}
+			
+			if(returnValid != true )
+			{
+				res.writeHead(200, {"Content-Type": "application/json","Access-Control-Allow-Origin":"*"
+					,"Access-Control-Allow-Methods":"POST, GET, PUT, DELETE, OPTIONS","Access-Control-Allow-Credentials":false
+					,"Access-Control-Max-Age":'86400'
+					,"Access-Control-Allow-Headers":"X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+				});
+				res.write(JSON.stringify({PASSED:true}));
+				res.end();
+			}
 		}
 		else if (req.method == 'POST')
 		{
