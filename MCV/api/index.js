942e6914a9a28a9ce4bdcca3ebf0830808218dea
mmassaly <massalymamadou@gmail.com>
2025-08-19 10:44:35 +0000

    Go

--- MCV/api/index.js
+++ MCV/api/index.js
@@ -147,7 +147,7 @@
 	}
 
 	setInterval(async () =>{ 
-		let awaitres = await doGetHTTPRequest("www.socket-serverv1.ms-associes-digital.com",undefined,"command=vide");
+		let awaitres = await doGetHTTPRequest( "www.socket-serverv1.ms-associes-digital.com",undefined,"command=vide");
 		//console.log(!awaitres?"Bad refreshing result":"Good refreshing result");
 		},10000);
 	
